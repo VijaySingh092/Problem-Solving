d54dbfbe3dba16c0c798761aebdfe250777f992a
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-08-02 22:55:12 +0530

    check power of 2

---
+++ 01_check_power_2.cpp
@@ -0,0 +1,24 @@
+// check if the number is power of 2 or not
+
+#include<iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n){
+    if(n>0 && (n&(n-1))==0){
+        return true;
+    }
+    else return false;
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(isPowerOfTwo(n)){
+        cout<<n<<" is a power of 2.";
+    }
+    else{
+        cout<<n<<" is not a power of 2";
+    }
+    return 0;
+}
